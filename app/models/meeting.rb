8fdfd0f68dbc019ccda5f49887d12cdbeb002dbf
Christa Hartsock <christa.hartsock@gmail.com>
2020-06-09 21:00:03 -0700

    Refactor header into shared partial
    Also adds test for date formatting

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,3 +1,7 @@
 class Meeting < ApplicationRecord
   belongs_to :police_district
+
+  def formatted_event_datetime
+    event_datetime&.strftime('%b %e, %Y @%l:%M%P')
+  end
 end
--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Meeting, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.formatted_event_datetime' do
+    it 'returns the formatted string' do
+      meeting = FactoryBot.build(:meeting, event_datetime: DateTime.new(2010,10,20,13,30,00))
+      expect(meeting.formatted_event_datetime).to eq('Oct 20, 2010 @ 1:30pm')
+    end
+
+    context 'when event_datetime is nil' do
+      it 'returns nil' do
+        meeting = FactoryBot.build(:meeting, event_datetime: nil)
+        expect(meeting.formatted_event_datetime).to be_nil
+      end
+    end
+  end
 end

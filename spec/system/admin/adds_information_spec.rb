000d4bba960224a674db6f2bcf41461046df48b9
Chris Arvin <carvin@gmail.com>
2020-07-06 00:30:24 -0700

    Add optional field to replace district budget label (#93)

--- spec/system/admin/adds_information_spec.rb
+++ spec/system/admin/adds_information_spec.rb
@@ -15,7 +15,8 @@ RSpec.describe 'information management' do
     click_on 'Add a new district'
 
     fill_in 'Name', with: 'BART PD'
-    fill_in 'Total Police FY 19-20 Budget', with: '160,000,000'
+    fill_in 'Total Law Enforcement FY 19-20 Budget', with: '160,000,000'
+    fill_in 'Optional alternate label for FY 19-20 budget', with: 'POLICE AND SHERIFF BUDGET'
     fill_in 'Total FY 19-20 City Budget', with: '1,000,000,000'
     fill_in 'Total FY 19-20 General Fund Budget', with: '50,000,000'
     fill_in 'Total FY 19-20 General Fund Spent on Police', with: '25,000,000'
@@ -42,6 +43,8 @@ RSpec.describe 'information management' do
 
     expect(page).to have_content('Transit')
     expect(page).to have_content('$1M')
+
+    expect(page).to have_text('POLICE AND SHERIFF BUDGET')
 
     within '[data-spec=general-fund]' do
       expect(page).to have_text "50%"
